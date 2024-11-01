8fe65ef4f5c26dd71da7787eeae3b65d0cd593fa
Andres Velez <andres.velez@atlantichealth.org>
2024-11-01 11:56:35 -0500

    agregar conteo de vehículos por marca en la lista

--- src/app/vehiculo/vehiculo-list/vehiculo.component.ts
+++ src/app/vehiculo/vehiculo-list/vehiculo.component.ts
@@ -2,6 +2,10 @@ import { Component, HostListener, OnInit } from '@angular/core';
 import { Vehiculo } from '../vehiculo';
 import { VehiculoService } from '../vehiculo.service';
 
+interface VehiculosPorMarca {
+  marca: string;
+  total: number;
+}
 @Component({
   selector: 'app-vehiculo-list',
   templateUrl: './vehiculo.component.html',
@@ -9,6 +13,8 @@ import { VehiculoService } from '../vehiculo.service';
 })
 export class VehiculoListComponent implements OnInit {
   vehiculos: Vehiculo[] = [];
+  vehiculosPorMarca: VehiculosPorMarca[] = [];
+
   constructor(private vehiculoService: VehiculoService) {}
 
   ngOnInit() {
@@ -21,6 +27,23 @@ export class VehiculoListComponent implements OnInit {
   getVehiculos(): void {
     this.vehiculoService.getVehiculos().subscribe((vehiculos) => {
       this.vehiculos = vehiculos;
+      this.contarVehiculosPorMarca();
+      console.log(this.vehiculosPorMarca);
     });
+  }
+
+  contarVehiculosPorMarca(): void {
+    const contador: { [marca: string]: number } = this.vehiculos.reduce(
+      (acc: { [marca: string]: number }, vehiculo) => {
+        acc[vehiculo.marca] = (acc[vehiculo.marca] || 0) + 1;
+        return acc;
+      },
+      {} as { [marca: string]: number }
+    );
+
+    this.vehiculosPorMarca = Object.keys(contador).map((marca) => ({
+      marca,
+      total: contador[marca],
+    }));
   }
 }
